f276332a18d892b944ca783a5c15fbedd24975c8
ovasylenko <lxvslnk@gmail.com>
2014-11-25 16:43:23 +0400

    typo

--- client-js/step-2.js
+++ client-js/step-2.js
@@ -12,7 +12,7 @@
                 mobiads.onload = function() {
                     window.mobiads.ads[payloadUrl](script);
                 };
-                s document.currentScript.parentNode.insertBefore(payload, script.nextSibling);
+                document.currentScript.parentNode.insertBefore(payload, script.nextSibling);
 
 
             // Set creative height
