76445184b772ed42a0dbd612ba2a016e939c0cf7
ovasylenko <lxvslnk@gmail.com>
2014-11-25 09:19:58 +0400

    added main.js

--- main.js
+++ main.js
@@ -33,15 +33,8 @@ app.get('/page/:page_id', function(req, res, next) {
 
 
 });
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function () {
 
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log('Example app listening at http://%s:%s', host, port)
-
-})
+var server = app.listen(process.env.PORT || 3000);
 
 var extractCss=function(domain,url){
     var fullUrl= domain+url;
